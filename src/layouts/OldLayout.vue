d0ebd255942dda40fae1e30f8855e5181b7dabde
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-03-21 23:07:30 +0300

    Add new layout and getting data from API (2).

---
+++ src/layouts/OldLayout.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>{{ appName }} {{ leftDrawerOpen }}</q-toolbar-title>
+
+        <div>
+          <strong>{{ appName }}</strong>, вер. {{ appVersion }}, {{ currentYear }} &copy;
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in mainMenuLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+import mainMenuLinks from "./../router/menu";
+
+const appName = process.env.appName;
+const appVersion = process.env.appVersion;
+
+const leftDrawerOpen = ref(true);
+
+const currentYear = () => (new Date()).getFullYear();
+const toggleLeftDrawer = () => leftDrawerOpen.value = !leftDrawerOpen.value;
+</script>
