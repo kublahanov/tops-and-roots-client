a4f1125f2053f2cfd5d1fa8cc88135165418e180
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-04-15 19:38:16 +0300

    Fix UserLayout.

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -1,42 +1,15 @@
 <template>
-  <!-- prettier-ignore -->
   <q-layout view="hHh lpR fFf">
     <q-header elevated :class="appSectionBgColor">
       <q-toolbar>
-        <q-btn dense icon="menu" @click="toggleLeftDrawer" />
+        <q-btn dense icon="menu" @click="toggleAppSectionDrawer" />
         <q-space></q-space>
-        <q-btn dense icon="menu" @click="toggleRightDrawer" />
+        <q-btn dense icon="menu" @click="toggleUserDrawer" />
       </q-toolbar>
-      <q-tabs align="left" dense inline-label no-caps outside-arrows class="tabs-margin">
-        <q-tab icon="o_home" class="q-tab--active" />
-      </q-tabs>
+      <MainTabs :hasTabs="false" :tabs="[]" />
     </q-header>
-    <q-drawer v-model="leftDrawerOpen" side="left" behavior="mobile" elevated>
-      <q-toolbar class="q-ma-sm">
-        <q-toolbar-title>{{ appName }}</q-toolbar-title>
-        <q-btn dense flat icon="close" @click="toggleLeftDrawer" class="q-mx-sm" />
-      </q-toolbar>
-      <q-list>
-        <MenuLink
-          v-for="link in appSectionMenuLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-    <q-drawer v-model="rightDrawerOpen" side="right" behavior="mobile" elevated>
-      <q-toolbar class="q-ma-sm">
-        <q-toolbar-title>{{ userSectionName }}</q-toolbar-title>
-        <q-btn dense flat icon="close" @click="toggleRightDrawer" class="q-mx-sm" />
-      </q-toolbar>
-      <q-list>
-        <MenuLink
-          v-for="link in profileMenuLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <AppSectionsDrawer v-model="isAppSectionDrawerOpen" />
+    <UserDrawer v-model="isUserDrawerOpen" />
     <q-page-container class="my-layout">
       <router-view />
     </q-page-container>
@@ -46,23 +19,26 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
-import { appSectionMenuLinks, profileMenuLinks } from "src/router/menu";
-import MenuLink from "components/MenuLink.vue";
 import { useMeta } from "quasar";
 import UserFooter from "components/UserFooter.vue";
+import MainTabs from "components/MainTabs.vue";
+import AppSectionsDrawer from "components/AppSectionsDrawer.vue";
+import UserDrawer from "components/UserDrawer.vue";
 
 /**
  * Флаги состояния левой и правой панели меню.
  */
-const leftDrawerOpen = ref(false);
-const rightDrawerOpen = ref(false);
+const isAppSectionDrawerOpen = ref(false);
+const isUserDrawerOpen = ref(false);
 
 /**
  * Переключатели состояния левой и правой панели меню.
  * @returns {boolean}
  */
-const toggleLeftDrawer = () => (leftDrawerOpen.value = !leftDrawerOpen.value);
-const toggleRightDrawer = () => (rightDrawerOpen.value = !rightDrawerOpen.value);
+const toggleAppSectionDrawer = () =>
+  (isAppSectionDrawerOpen.value = !isAppSectionDrawerOpen.value);
+const toggleUserDrawer = () =>
+  (isUserDrawerOpen.value = !isUserDrawerOpen.value);
 
 /**
  * Константы.
